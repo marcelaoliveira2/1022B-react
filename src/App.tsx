aaa7f228e34f53474a632f16eb7002a79376f75e
marcelaoliveira2 <marcela.oliveira2@estudante.ifms.edu.br>
2024-03-13 10:15:41 -0400

    Meu primeiro componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
+import MeuComponente from './componentes/MeuComponente'
 function App() {
   let nome = "Marcela"
   let sobrenome = "Santos de Oliveira"
   return (
-    <div>
+    <>
+    <MeuComponente></MeuComponente>
      <h1 className="nome"> Nome: {nome}</h1>
      <p className="sobrenome"> {sobrenome}</p>
-    </div>
+    </>
   )// Retorna JSX - JavaScript XML
 }
 export default App
