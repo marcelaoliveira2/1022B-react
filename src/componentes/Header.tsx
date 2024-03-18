a9fbffb5196e32084c1a0bece17db1b3150b2c51
marcelaoliveira2 <marcela.oliveira2@estudante.ifms.edu.br>
2024-03-18 11:44:34 -0400

    aleatório

---
+++ src/componentes/Header.tsx
@@ -0,0 +1,25 @@
+import './Header.css'
+function Header(){
+    return(
+        <header>
+            <div className='logo'>
+                1022 B
+            </div>
+            <nav>
+                <ul>
+                <li>
+                    <a href="http://">Home</a>
+                    </li>
+                <li>
+                    <a href="http://">Listagem</a>
+                    </li>
+                <li>
+                    <a href="http://">Informações</a>
+                    </li>
+                    </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
